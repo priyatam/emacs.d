bb444ffe62f23c5fef98e9d7c3028d8bd186f56d
Priyatam Mudivarti <priyatam@gmail.com>
2016-03-07 18:54:47 -0800

    modularize clojure packages

---
+++ src/clojure.el
@@ -0,0 +1,95 @@
+(require 'cider-mode)
+(require 'cider-eval-sexp-fu)
+(require 'clojure-mode)
+(require 'clj-refactor)
+(require 'cider-eval-sexp-fu)
+
+;; Cider
+
+(global-company-mode)
+
+(add-hook 'after-init-hook 'global-company-mode)
+(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
+(add-hook 'clojure-mode-hook #'rainbow-delimiters-mode)
+(add-hook 'clojure-mode-hook #'paredit-mode)
+(add-hook 'cider-repl-mode-hook #'paredit-mode)
+(add-hook 'clojure-mode-hook #'eldoc-mode)
+
+(setq cider-prompt-for-symbol nil)
+(setq cider-prompt-save-file-on-load 'always-save)
+(setq nrepl-log-messages t)
+(setq nrepl-hide-special-buffers t)
+(setq cider-repl-pop-to-buffer-on-connect nil)
+(setq cider-show-error-buffer 'except-in-repl)
+(setq cider-stacktrace-default-filters '(java repl tooling dup))
+(setq cider-switch-to-repl-command #'cider-switch-to-current-repl-buffer)
+(setq cider-repl-display-in-current-window t)
+(setq cider-repl-display-help-banner nil)
+(setq cider-interactive-eval-result-prefix ";; => ")
+(setq cider-font-lock-dynamically '(macro core function var))
+(setq cider-overlays-use-font-lock t)
+(setq cider-pprint-fn "fipp.edn/pprint")
+
+(show-paren-mode 1)
+
+(eval-after-load "cider"
+  '(define-key cider-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc))
+
+;; Clojure
+
+(add-to-list 'auto-mode-alist '("\\.boot\\'" . clojure-mode))
+
+(add-hook 'clojure-mode-hook (lambda () (whitespace-mode t)))
+(add-hook 'clojurescript-mode-hook (lambda () (whitespace-mode t)))
+(setq whitespace-style '(face lines-tail trailing))
+(setq whitespace-line-column 84)
+
+;; Coding Standards
+
+(define-clojure-indent
+  (expect 'defun)
+  (expect-let 'defun)
+  (given 'defun)
+  (context 1)
+  (freeze-time 1)
+  (redef-state 1)
+  (from-each 1)
+  (component 'defun)
+  (div 'defun)
+  (span 'defun)
+  (form 'defun)
+  (a 'defun)
+  (ul 'defun)
+  (li 'defun)
+  (input 'defun)
+  (h1 'defun)
+  (h2 'defun)
+  (h3 'defun)
+  (h4 'defun)
+  (h5 'defun)
+  (h6 'defun)
+  (button 'defun)
+  (textarea 'defun))
+
+(defun clj-refactor-mode-hook ()
+  (clj-refactor-mode 1)
+  (yas-minor-mode 1) ; for adding require/use/import
+  (cljr-add-keybindings-with-prefix "C-c C-m"))
+
+(add-hook 'clojure-mode-hook #'clj-refactor-mode-hook)
+
+(eval-after-load 'clojure-mode
+  '(progn
+     (define-key clojure-mode-map (kbd "C-c C-h") #'clojure-cheatsheet)))
+
+(setq-default fill-column 80)
+
+;; Flycheck
+(eval-after-load 'flycheck '(flycheck-clojure-setup))
+;;(add-hook 'after-init-hook #'global-flycheck-mode)
+;;(eval-after-load 'flycheck
+;;  '(setq flycheck-display-errors-function #'flycheck-pos-tip-error-messages))
+
+;; Hoplon
+(add-to-list 'auto-mode-alist '("\\.cljs\\.hl\\'" . clojure-mode))
+(add-to-list 'auto-mode-alist '("\\.boot\'" . clojure-mode))
