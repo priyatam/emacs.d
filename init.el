36f4ad2132f6c246e33543592185b152a6fafeb2
Priyatam Mudivarti <priyatam@facjure.com>
2018-04-16 14:38:11 -0700

    cleanup

--- init.el
+++ init.el
@@ -43,17 +43,20 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(css-indent-offset 2)
- '(custom-enabled-themes '(misterioso))
+ '(custom-enabled-themes (quote (misterioso)))
  '(custom-safe-themes
    (quote
 	("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "8553bdd21237bcc7d9cfabd93c1525af9ac4a84d5ab1435e6474f9fd4f81d69c" "bf25a2d5c2eddc36b2ee6fc0342201eb04ea090e637562c95b3b6e071216b524" default)))
  '(fci-rule-color "#d6d6d6")
- '(js2-basic-offset 2)
+ '(js-indent-level 2)
  '(js2-bounce-indent-p t)
  '(js2-missing-semi-one-line-override t)
  '(js2-strict-missing-semi-warning nil)
  '(jsx-indent-level 2)
  '(jsx-use-flymake t)
+ '(package-selected-packages
+   (quote
+	(yaml-mode writeroom-mode web-mode use-package smex smart-mode-line scss-mode rainbow-delimiters projectile prodigy pos-tip paredit pallet markdown-mode magit js2-mode inf-clojure idle-highlight-mode helm-dash go-mode git-timemachine flycheck-cask expand-region exec-path-from-shell emmet-mode drag-stuff company)))
  '(web-mode-attr-indent-offset 2)
  '(web-mode-code-indent-offset 2)
  '(web-mode-css-indent-offset 2)
